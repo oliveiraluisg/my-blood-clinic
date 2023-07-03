4fb3114e58c236cc6a4cc829041276adcf92583f
oliveiraluisg <luisg.araujooliveira@gmail.com>
2023-07-02 22:25:28 -0300

    deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFireStorageModule,
+    AngularFirestoreModule.enablePersistence(),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+    enabled: environment.production,
+    registrationStrategy: 'registerWhenStable:3000'
+    })
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAy1QY5FSUj3aWxt-fjjA1_Cx3qaxj0chM",
+    authDomain: "blood-clinic.firebaseapp.com",
+    projectId: "blood-clinic",
+    storageBucket: "blood-clinic.appspot.com",
+    messagingSenderId: "952164685975",
+    appId: "1:952164685975:web:215f74ba06e962bac471d2"
+  }
 };
